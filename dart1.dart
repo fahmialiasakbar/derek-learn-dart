e18b5997342e6ae21ff4e092542477af9c53b71e
Fahmi Akbar <fahmialiasakbar@gmail.com>
2021-10-24 06:26:50 +0700

    Variable

---
+++ dart1.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+void main() {
+  print('Hello Dart');
+  var name = "Fahmi";
+  String name2 = "Akbar";
+  dynamic anything = 20;
+  anything = "Teknik Informatika";
+
+  late int var1;
+  int age = 45;
+  double money = 123.45;
+  bool canVote = true;
+  print("Is Bool ${canVote is bool}");
+
+  int? imNull;
+  print("${imNull}");
+  final PI = 3.14;
+}
